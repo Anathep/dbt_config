a578efadd6443e786aa35332b42b783c6163c8cd
anaisth.lewagon@gmail.com <anaisth.lewagon@gmail.com>
2024-05-07 14:33:53 +0000

    update schema

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,8 +1,13 @@
-WITH tce as
+WITH cte as
 (SELECT *
 FROM {{ ref('int_orders_margin') }}
 JOIN {{ ref('stg_raw__ship') }}
 USING (orders_id))
-SELECT orders_id, date_date, ROUND(margin + shipping_fee - ship_cost - log_cost,2) AS operational_margin, quantity, revenue, margin
-FROM tce
+SELECT orders_id, 
+    date_date, 
+    ROUND(margin + shipping_fee - ship_cost - log_cost,2) AS operational_margin, 
+    quantity, 
+    revenue, 
+    margin
+FROM cte
 ORDER BY orders_id DESC
