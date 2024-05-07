752736c4f046e8760bb1da119712124cca4e25d1
anaisth.lewagon@gmail.com <anaisth.lewagon@gmail.com>
2024-05-07 12:53:14 +0000

    new table (orders operational)

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,8 @@
+WITH tce as
+(SELECT *
+FROM {{ ref('int_orders_margin') }}
+JOIN {{ ref('stg_raw__ship') }}
+USING (orders_id))
+SELECT orders_id, date_date, ROUND(margin + shipping_fee - ship_cost - log_cost,2) AS operational_margin, quantity
+FROM tce
+ORDER BY orders_id DESC
