8177bc0a2af037aab22239993d4cc9b584bf8ea1
anaisth.lewagon@gmail.com <anaisth.lewagon@gmail.com>
2024-05-07 13:32:11 +0000

    finance days model

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -3,6 +3,6 @@ WITH tce as
 FROM {{ ref('int_orders_margin') }}
 JOIN {{ ref('stg_raw__ship') }}
 USING (orders_id))
-SELECT orders_id, date_date, ROUND(margin + shipping_fee - ship_cost - log_cost,2) AS operational_margin, quantity
+SELECT orders_id, date_date, ROUND(margin + shipping_fee - ship_cost - log_cost,2) AS operational_margin, quantity, revenue, margin
 FROM tce
 ORDER BY orders_id DESC
