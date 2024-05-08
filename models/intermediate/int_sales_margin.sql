807369c77f9d3a655e594e0961a3c65fa8fb81d1
Anaïs THEPAUT <anaisth.lewagon@gmail.com>
2024-05-08 12:26:41 +0000

    installed packages

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,5 +4,5 @@ WITH tce as
 FROM {{ref("stg_raw__product")}}
 JOIN {{ref("stg_raw__sales")}}
 USING (products_id))
-SELECT *, ROUND(revenue - purchase_cost,2) AS margin
+SELECT *, ROUND(revenue - purchase_cost,2) AS margin, {{ margin_percent(revenue,purchase_cost)}} AS margin_percent
 FROM tce
